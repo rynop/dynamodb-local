13c9b44971b6d9343b4cbbe5b2da7f5dc83b886e
ryanp <ryanp@doapps.com>
2015-08-07 09:22:11 -0500

    replace ES6 Promise with Q

--- index.js
+++ index.js
@@ -1,102 +1,109 @@
 "use strict";
 
-var os = require('os'),
-    spawn = require('child_process').spawn,
-    fs = require('fs'),
-    http = require('http'),
-    tar = require('tar'),
-    zlib = require('zlib'),
-    path = require('path'),
-    mkdirp = require('mkdirp');
+var os     = require('os'),
+    spawn  = require('child_process').spawn,
+    fs     = require('fs'),
+    http   = require('http'),
+    tar    = require('tar'),
+    zlib   = require('zlib'),
+    path   = require('path'),
+    mkdirp = require('mkdirp'),
+    Q      = require('q');
 
 var JARNAME = 'DynamoDBLocal.jar';
 
 var tmpDynamoLocalDirDest = os.tmpdir() + 'dynamodb-local',
-    runningProcesses = {},
-    DynamoDbLocal = {
-      /**
-       *
-       * @param port
-       * @param dbPath if omitted will use in memory
-       * @param additionalArgs
-       * @returns {Promise.<ChildProcess>}
-       */
-      launch: function (port, dbPath, additionalArgs) {
-        if (runningProcesses[port]) {
-          return new Promise(function (resolve, reject) {
-            resolve(runningProcesses[port]);
-          });
+    runningProcesses      = {},
+    DynamoDbLocal         = {
+        /**
+         *
+         * @param port
+         * @param dbPath if omitted will use in memory
+         * @param additionalArgs
+         * @returns {Promise.<ChildProcess>}
+         */
+        launch: function (port, dbPath, additionalArgs) {
+            if (runningProcesses[port]) {
+                return Q.fcall(function () {
+                    return runningProcesses[port];
+                });
+            }
+
+            if (!additionalArgs) {
+                additionalArgs = [];
+            }
+            else if (Array.isArray(additionalArgs)) {
+                additionalArgs = [additionalArgs];
+            }
+
+            if (!dbPath) {
+                additionalArgs.push('-inMemory');
+            }
+            else {
+                additionalArgs.push('-dbPath', dbPath);
+            }
+
+            return installDynamoDbLocal()
+                .then(function () {
+                    var args = [
+                        '-Djava.library.path=./DynamoDBLocal_lib',
+                        '-jar',
+                        JARNAME,
+                        '-port',
+                        port
+                    ];
+                    args     = args.concat(additionalArgs);
+
+                    var child = spawn('java', args, {
+                        cwd: tmpDynamoLocalDirDest,
+                        env: process.env,
+                        stdio: ['pipe', 'pipe', process.stderr]
+                    });
+
+                    if (!child.pid) throw new Error("Unable to launch DyanmoDBLocal process");
+
+                    child
+                        .on('error', function (err) {
+                            console.log("local DynamoDB start error", err);
+                            throw new Error("Local DynamoDB failed to start. ");
+                        })
+                        .on('close', function (code) {
+                            if (code !== null && code !== 0) {
+                                console.log('Local DynamoDB failed to start with code', code);
+                            }
+                        });
+
+                    runningProcesses[port] = child;
+
+                    console.log("DynamoDbLocal(" + child.pid + ") started on port", port, "via java", args.join(' '), "from CWD", tmpDynamoLocalDirDest);
+
+                    return child;
+                });
+        },
+        stop: function (port) {
+            if (runningProcesses[port]) {
+                runningProcesses[port].kill('SIGKILL');
+                delete runningProcesses[port];
+            }
+        },
+        relaunch: function (port, db) {
+            this.stop(port);
+            this.launch(port, db);
         }
-
-        if (!additionalArgs) {
-          additionalArgs = [];
-        }
-        else if (Array.isArray(additionalArgs)) {
-          additionalArgs = [additionalArgs];
-        }
-
-        if (!dbPath) {
-          additionalArgs.push('-inMemory');
-        }
-        else {
-          additionalArgs.push('-dbPath', dbPath);
-        }
-
-        return installDynamoDbLocal()
-            .then(function () {
-              var args = [
-                '-Djava.library.path=./DynamoDBLocal_lib',
-                '-jar',
-                JARNAME,
-                '-port',
-                port
-              ];
-              args = args.concat(additionalArgs);
-
-              var child = spawn('java', args, {cwd: tmpDynamoLocalDirDest, env: process.env, stdio:['pipe', 'pipe', process.stderr]});
-
-              if (!child.pid) throw new Error("Unable to launch DyanmoDBLocal process");
-
-              child
-                  .on('error', function (err) {
-                    console.log("local DynamoDB start error", err);
-                    throw new Error("Local DynamoDB failed to start. ");
-                  })
-                  .on('close', function (code) {
-                    if (code !== null && code !== 0) {
-                      console.log('Local DynamoDB failed to start with code', code);
-                    }
-                  });
-
-              runningProcesses[port] = child;
-
-              console.log("DynamoDbLocal(" + child.pid + ") started on port", port, "via java", args.join(' '), "from CWD", tmpDynamoLocalDirDest);
-
-              return child;
-            });
-      },
-      stop: function (port) {
-        if (runningProcesses[port]) {
-          runningProcesses[port].kill('SIGKILL');
-          delete runningProcesses[port];
-        }
-      },
-      relaunch: function (port, db) {
-        this.stop(port);
-        this.launch(port, db);
-      }
     };
 
 module.exports = DynamoDbLocal;
 
 function installDynamoDbLocal() {
-  console.log("Checking for ", tmpDynamoLocalDirDest);
-  return new Promise(function (resolve, reject) {
+    console.log("Checking for ", tmpDynamoLocalDirDest);
+    var deferred = Q.defer();
+
     try {
-      if (fs.existsSync(tmpDynamoLocalDirDest + '/' + JARNAME)) {
-        resolve();
-        return;
-      }
+        if (fs.existsSync(tmpDynamoLocalDirDest + '/' + JARNAME)) {
+            return Q.fcall(function () {
+                return true;
+            });
+        }
     } catch (e) {
     }
 
@@ -107,31 +114,32 @@ function installDynamoDbLocal() {
 
     http
         .get('http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz', function (response) {
-          if (302 != response.statusCode) {
-            reject("Error getting DyanmoDb local latest tar.gz location: " + response.statusCode);
-          }
+            if (302 != response.statusCode) {
+                deferred.reject(new Error("Error getting DyanmoDb local latest tar.gz location: " + response.statusCode));
+            }
 
-          http
-              .get(response.headers['location'], function (redirectResponse) {
-                if (200 != redirectResponse.statusCode) {
-                  reject("Error getting DyanmoDb local latest tar.gz location " + response.headers['location'] + ": " + redirectResponse.statusCode);
-                }
-                redirectResponse
-                    .pipe(zlib.Unzip())
-                    .pipe(tar.Extract({path: tmpDynamoLocalDirDest}))
-                    .on('end', function () {
-                      resolve();
-                    })
-                    .on('error', function (err) {
-                      reject(err);
-                    });
-              })
-              .on('error', function (e) {
-                reject(e.message);
-              });
+            http
+                .get(response.headers['location'], function (redirectResponse) {
+                    if (200 != redirectResponse.statusCode) {
+                        deferred.reject(new Error("Error getting DyanmoDb local latest tar.gz location " + response.headers['location'] + ": " + redirectResponse.statusCode));
+                    }
+                    redirectResponse
+                        .pipe(zlib.Unzip())
+                        .pipe(tar.Extract({path: tmpDynamoLocalDirDest}))
+                        .on('end', function () {
+                            deferred.resolve();
+                        })
+                        .on('error', function (err) {
+                            deferred.reject(err);
+                        });
+                })
+                .on('error', function (e) {
+                    deferred.reject(e);
+                });
         })
         .on('error', function (e) {
-          reject(e.message);
+            deferred.reject(e);
         });
-  });
+
+    return deferred.promise;
 }
